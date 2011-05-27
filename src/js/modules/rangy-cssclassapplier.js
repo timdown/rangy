6a4c37414f996f45a7e5276850a4aec799aad787
Tim Down <tim@timdown.co.uk>
2011-05-27 00:20:58 +0000

    Ported fix and test for issue 50 from 1.1 branch

--- src/js/modules/rangy-cssclassapplier.js
+++ src/js/modules/rangy-cssclassapplier.js
@@ -72,6 +72,17 @@ rangy.createModule("CssClassApplier", function(api, module) {
         parent.removeChild(el);
     }
 
+    function rangeSelectsAnyText(range, textNode) {
+        var textRange = range.cloneRange();
+        textRange.selectNodeContents(textNode);
+
+        var intersectionRange = textRange.intersection(range);
+        var text = intersectionRange ? intersectionRange.toString() : "";
+        textRange.detach();
+
+        return text != "";
+    }
+
     function elementsHaveSameNonClassAttributes(el1, el2) {
         if (el1.attributes.length != el2.attributes.length) return false;
         for (var i = 0, len = el1.attributes.length, attr1, attr2, name; i < len; ++i) {
@@ -443,7 +454,9 @@ rangy.createModule("CssClassApplier", function(api, module) {
 
         applyToRange: function(range) {
             range.splitBoundaries();
-            var textNodes = range.getNodes([3]);
+            var textNodes = range.getNodes([3], function(textNode) {
+                return rangeSelectsAnyText(range, textNode);
+            });
 
             if (textNodes.length) {
                 var textNode;
@@ -532,9 +545,7 @@ rangy.createModule("CssClassApplier", function(api, module) {
         isAppliedToRange: function(range) {
             var textNodes = range.getNodes( [3] );
             for (var i = 0, len = textNodes.length, selectedText; i < len; ++i) {
-                selectedText = this.getTextSelectedByRange(textNodes[i], range);
-                log.warn("text node: '" + textNodes[i].data + "', selectedText: '" + selectedText + "'");
-                if (selectedText != "" && !(this.ignoreWhiteSpace && dom.isWhiteSpaceNode(textNodes[i])) && !this.getAncestorWithClass(textNodes[i])) {
+                if (rangeSelectsAnyText(range, textNodes[i]) && !this.getAncestorWithClass(textNodes[i])) {
                     return false;
                 }
             }
