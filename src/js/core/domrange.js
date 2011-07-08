896d3835cfd1e1ac62ccd1247117681d5b0d88c6
Tim Down <tim@timdown.co.uk>
2011-07-08 00:02:12 +0000

    Add containsRange() and containsNodeText() methods to Range

--- src/js/core/domrange.js
+++ src/js/core/domrange.js
@@ -168,7 +168,7 @@ rangy.createModule("DomRange", function(api, module) {
 
     /*----------------------------------------------------------------------------------------------------------------*/
 
-    // RangeIterator code borrows from IERange by Tim Ryan (http://github.com/timcameronryan/IERange)
+    // RangeIterator code partially borrows from IERange by Tim Ryan (http://github.com/timcameronryan/IERange)
 
     /**
      * @constructor
@@ -785,7 +785,8 @@ rangy.createModule("DomRange", function(api, module) {
                 return frag;
             },
 
-            // This follows the Mozilla model whereby a node that borders a range is not considered to intersect with it
+            // touchingIsIntersecting determines whether this method considers a node that borders a range intersects
+            // with it (as in WebKit) or not (as in Gecko pre-1.9, and the default)
             intersectsNode: function(node, touchingIsIntersecting) {
                 assertNotDetached(this);
                 assertRangeValid(this);
@@ -856,6 +857,26 @@ rangy.createModule("DomRange", function(api, module) {
 
             containsNodeContents: function(node) {
                 return this.comparePoint(node, 0) >= 0 && this.comparePoint(node, getEndOffset(node)) <= 0;
+            },
+
+            containsRange: function(range) {
+                return this.intersection(range).equals(range);
+            },
+
+            containsNodeText: function(node) {
+                var nodeRange = this.cloneRange();
+                nodeRange.selectNode(node);
+                var textNodes = nodeRange.getNodes([3]);
+                if (textNodes.length > 0) {
+                    nodeRange.setStart(textNodes[0], 0);
+                    var lastTextNode = textNodes.pop();
+                    nodeRange.setEnd(lastTextNode, lastTextNode.length);
+                    var contains = this.containsRange(nodeRange);
+                    nodeRange.detach();
+                    return contains;
+                } else {
+                    return this.containsNodeContents(node);
+                }
             },
 
             splitBoundaries: function() {
