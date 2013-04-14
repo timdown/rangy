13ce8c9c1b69390432e5c61c7c5d17f31c114e8f
Tim Down <tim@timdown.co.uk>
2013-04-14 23:49:46 +0000

    Changed so that Rangy's own DOM4-compliant implementation is always used for selectNodeContents() (issue 162)

--- src/js/core/wrappedrange.js
+++ src/js/core/wrappedrange.js
@@ -179,20 +179,11 @@ rangy.createModule("WrappedRange", function(api, module) {
 
             /*--------------------------------------------------------------------------------------------------------*/
 
-            // Test for and correct Firefox 2 behaviour with selectNodeContents on text nodes: it collapses the range to
-            // the 0th character of the text node
-            range.selectNodeContents(testTextNode);
-            if (range.startContainer == testTextNode && range.endContainer == testTextNode &&
-                    range.startOffset == 0 && range.endOffset == testTextNode.length) {
-                rangeProto.selectNodeContents = function(node) {
-                    this.nativeRange.selectNodeContents(node);
-                    updateRangeProperties(this);
-                };
-            } else {
-                rangeProto.selectNodeContents = function(node) {
-                    this.setStartAndEnd(node, 0, dom.getNodeLength(node));
-                };
-            }
+            // Always use DOM4-compliant selectNodeContents implementation: it's simpler and less code than testing
+            // whether the native implementation can be trusted
+            rangeProto.selectNodeContents = function(node) {
+                this.setStartAndEnd(node, 0, dom.getNodeLength(node));
+            };
 
             /*--------------------------------------------------------------------------------------------------------*/
 
