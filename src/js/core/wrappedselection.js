fc34ad18f05d89e3d979f07d30be59f6c590ddd3
Tim <tim@widgit.com>
2014-08-01 23:43:21 +0100

    Added disgusting browser sniff to avoid "Discontiguous selection is not supported" console warning in recent Chrome

--- src/js/core/wrappedselection.js
+++ src/js/core/wrappedselection.js
@@ -153,21 +153,25 @@
                 collapsedNonEditableSelectionsSupported = (sel.rangeCount == 1);
                 sel.removeAllRanges();
 
-                // Test whether the native selection is capable of supporting multiple ranges
+                // Test whether the native selection is capable of supporting multiple ranges.
                 if (!selectionHasMultipleRanges) {
-                    var r2 = r1.cloneRange();
-                    r1.setStart(textNode, 0);
-                    r2.setEnd(textNode, 3);
-                    r2.setStart(textNode, 2);
-                    sel.addRange(r1);
-
-                    // Next line causes Chrome 36 (and presumably later versions) to print a console error of
-                    // "Discontiguous selection is not supported.". There's nothing we can do about this while retaining
-                    // this feature test.
-                    // https://code.google.com/p/chromium/issues/detail?id=353069#c4
-                    sel.addRange(r2);
-
-                    selectionSupportsMultipleRanges = (sel.rangeCount == 2);
+                    // Doing the original feature test here in Chrome 36 (and presumably later versions) prints a
+                    // console error of "Discontiguous selection is not supported." that cannot be suppressed. There's
+                    // nothing we can do about this while retaining the feature test so we have to resort to a browser
+                    // sniff. I'm not happy about it. See
+                    // https://code.google.com/p/chromium/issues/detail?id=399791
+                    var chromeMatch = window.navigator.appVersion.match(/Chrome\/(.*?) /);
+                    if (chromeMatch && parseInt(chromeMatch[1]) >= 36) {
+                        selectionSupportsMultipleRanges = false;
+                    } else {
+                        var r2 = r1.cloneRange();
+                        r1.setStart(textNode, 0);
+                        r2.setEnd(textNode, 3);
+                        r2.setStart(textNode, 2);
+                        sel.addRange(r1);
+                        sel.addRange(r2);
+                        selectionSupportsMultipleRanges = (sel.rangeCount == 2);
+                    }
                 }
 
                 // Clean up
